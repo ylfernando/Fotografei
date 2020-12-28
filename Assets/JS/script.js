ab1b86f2a83f3d9de5124cd6075d40dc27f61318
Fernando Freitas <ylfernando3@gmail.com>
2020-12-28 18:30:37 -0300

    Adicionando scroll suave e colocando links no footer

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,7 @@
-$('#ModalLogin').on('click', function() {
-
-})
+/*$('.scroll').on('click', function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href'), targetOffset = $(id).offset().top;
+		$('html', 'body').animate( {
+			scrollTop: targetOffset
+		}, 600);
+	});
