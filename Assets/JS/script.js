de8909e884c2050efd3e68acba09a682b3e14e46
Fernando Freitas <ylfernando1@gmail.com>
2021-01-06 16:28:37 -0300

    Enviando scroll com jQuery

--- Assets/JS/script.js
+++
@@ -1,7 +0,0 @@
-/*$('.scroll').on('click', function(e) {
-		e.preventDefault();
-		var id = $(this).attr('href'), targetOffset = $(id).offset().top;
-		$('html', 'body').animate( {
-			scrollTop: targetOffset
-		}, 600);
-	});
